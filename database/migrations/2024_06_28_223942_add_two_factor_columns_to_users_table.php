12e8d8710650856fe4c94daf4759f1d1ad352556
Nabil-nl <nabilouhmida9@gmail.com>
2024-06-30 23:10:08 +0100

    update

--- database/migrations/2024_06_28_223942_add_two_factor_columns_to_users_table.php
+++ database/migrations/2024_06_28_223942_add_two_factor_columns_to_users_table.php
@@ -5,42 +5,31 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Laravel\Fortify\Fortify;
 
-return new class extends Migration
+class AddTwoFactorColumnsToUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('two_factor_secret')
-                ->after('password')
-                ->nullable();
+            if (!Schema::hasColumn('users', 'two_factor_secret')) {
+                $table->text('two_factor_secret')->nullable()->after('password');
+            }
 
-            $table->text('two_factor_recovery_codes')
-                ->after('two_factor_secret')
-                ->nullable();
+            if (!Schema::hasColumn('users', 'two_factor_recovery_codes')) {
+                $table->text('two_factor_recovery_codes')->nullable()->after('two_factor_secret');
+                $table->index('two_factor_recovery_codes');
+            }
 
-            if (Fortify::confirmsTwoFactorAuthentication()) {
-                $table->timestamp('two_factor_confirmed_at')
-                    ->after('two_factor_recovery_codes')
-                    ->nullable();
+            if (!Schema::hasColumn('users', 'two_factor_confirmed_at')) {
+                $table->timestamp('two_factor_confirmed_at')->nullable()->after('two_factor_recovery_codes');
             }
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn(array_merge([
-                'two_factor_secret',
-                'two_factor_recovery_codes',
-            ], Fortify::confirmsTwoFactorAuthentication() ? [
-                'two_factor_confirmed_at',
-            ] : []));
+            $table->dropIndex(['two_factor_recovery_codes']);
+            $table->dropColumn(['two_factor_secret', 'two_factor_recovery_codes', 'two_factor_confirmed_at']);
         });
     }
-};
+}
--- database/migrations/2024_06_29_234705_add_user_id_to_notes_table.php
+++ database/migrations/2024_06_29_234705_add_user_id_to_notes_table.php
@@ -6,34 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 class AddUserIdToNotesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('notes', function (Blueprint $table) {
-            if (!Schema::hasColumn('notes', 'user_id')) {
-                $table->unsignedBigInteger('user_id')->nullable()->after('category_id');
-                // Optional: Add foreign key constraint
-                $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            }
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('notes', function (Blueprint $table) {
-            if (Schema::hasColumn('notes', 'user_id')) {
-                $table->dropForeign(['user_id']);
-                $table->dropColumn('user_id');
-            }
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
         });
     }
 }
